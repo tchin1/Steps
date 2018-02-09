3c71c8074372fa84fd66b51a390296210458f759
kcooper5 <kcooper5@live.maryville.edu>
2018-02-09 13:45:22 -0600

    Changes

--- Music Interval App/Music Interval App/ViewController.swift
+++ Music Interval App/Music Interval App/ViewController.swift
@@ -13,8 +13,10 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var musicEffect: AVAudioPlayer = AVAudioPlayer()
-    
-    
+    var musicEffect2: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect3: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect4: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect5: AVAudioPlayer = AVAudioPlayer()
     
 
     override func viewDidLoad() {
@@ -23,12 +25,49 @@ class ViewController: UIViewController {
         let soundC3 = Bundle.main.path(forResource: "C3", ofType: ".mp3")
         do {
             try musicEffect = AVAudioPlayer(contentsOf:URL(fileURLWithPath: soundC3!))
-        
         }
         catch {
             print(error)
-        
         }
+      
+        
+        let soundDb =
+            Bundle.main.path(forResource: "Db", ofType: ".mp3")
+        do {
+            try musicEffect2 = AVAudioPlayer(contentsOf:URL(fileURLWithPath: soundDb!))
+        }
+        catch {
+            print(error)
+        }
+        let soundD =
+            Bundle.main.path(forResource: "D", ofType: ".mp3")
+        do {
+            try musicEffect3 = AVAudioPlayer(contentsOf:URL(fileURLWithPath: soundD!))
+        }
+        catch {
+            print(error)
+        }
+        let soundEb =
+            Bundle.main.path(forResource: "Eb", ofType: ".mp3")
+        do {
+            try musicEffect4 = AVAudioPlayer(contentsOf:URL(fileURLWithPath: soundEb!))
+        }
+        catch {
+            print(error)
+        }
+        let soundE =
+            Bundle.main.path(forResource: "E", ofType: ".mp3")
+        do {
+            try musicEffect5 = AVAudioPlayer(contentsOf:URL(fileURLWithPath: soundE!))
+        }
+        catch {
+            print(error)
+        }
+        
+        
+        
+        
+        
         
     }
     
@@ -43,8 +82,20 @@ class ViewController: UIViewController {
     
     @IBAction func C3(_ sender: Any) {
         musicEffect.play()
-        
     }
+    @IBAction func Db(_ sender: Any) {
+        musicEffect2.play()
+    }
+    @IBAction func D(_ sender: Any) {
+        musicEffect3.play()
+    }
+    @IBAction func Eb(_ sender: Any) {
+        musicEffect4.play()
+    }
+    @IBAction func E(_ sender: Any) {
+        musicEffect5.play()
+    }
+    
     
 
 }
